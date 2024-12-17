de6853150f1e0c2e6143601ed1fabb83caa3050f
Siddiqui Taha <tahasiddiqui541@gmail.com>
2024-12-17 15:53:31 +0530

    State Provider logic implemented

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -5,11 +5,16 @@ import 'package:riverpod_trial/provider_example.dart';
 class Homescreen extends ConsumerWidget {
  const Homescreen({super.key});
 
+ void updatevalue(WidgetRef ref, int value) {
+  ref.read(countProvider.notifier).update((state) => value);
+ }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     
     final text1 = ref.watch(readprovider);
     final text2 = ref.read(readprovider);
+    final count = ref.watch(countProvider) ?? '0';
 
     return Scaffold(
       backgroundColor: Colors.blueGrey.shade300,
@@ -25,9 +30,21 @@ class Homescreen extends ConsumerWidget {
         children: [
           const SizedBox(height: 20,),
           Text("Simple Provider Watch Example : $text1"),
-          Text("Simple Provider Read Example : $text2")
+          Text("Simple Provider Read Example : $text2"),
+          const SizedBox(height: 50,),
+          Center(child: Text(count.toString(), style: TextStyle(fontSize: 20),)),
+          //TextButton(onPressed: () => ref.read(countProvider.notifier).update((state) => value), child: Icon(Icons.add))
+          Row( 
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(onPressed: () => updatevalue(ref, (int.tryParse(count.toString()) ?? 0) +1), child: Icon(Icons.add), ),
+              TextButton(onPressed: () => updatevalue(ref, (int.tryParse(count.toString()) ?? 0) -1), child: Icon(Icons.remove), ),
+            ],
+          ),
+          
         ],
       ),
+      
     );
   }
 }
