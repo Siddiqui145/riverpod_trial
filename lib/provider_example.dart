de4a30c9c05ce921d53e64e66421f151a72a1eb1
Siddiqui Taha <tahasiddiqui541@gmail.com>
2024-12-17 16:26:20 +0530

    Implemented Future Provider

--- lib/provider_example.dart
+++ lib/provider_example.dart
@@ -1,5 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:riverpod_trial/models/api.dart';
 
 final readprovider = Provider<String>((ref) =>  "Hello, Riverpod");
 
 final countProvider = StateProvider<int?>((ref) => null );
+
+final fetchProvider = FutureProvider((ref) async {
+  const url = "https://official-joke-api.appspot.com/random_joke";
+  final response = await http.get(Uri.parse(url));
+  return Joke.fromJson(jsonDecode(response.body));
+} );
