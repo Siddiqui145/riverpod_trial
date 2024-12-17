b27f56cade736d2146c43f5f39c7e28195a7d39d
Siddiqui Taha <tahasiddiqui541@gmail.com>
2024-12-17 23:56:00 +0530

    Fetching Stream Data

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -17,6 +17,7 @@ class Homescreen extends ConsumerWidget {
     final count = ref.watch(countProvider) ?? '0';
     final result = ref.watch(fetchProvider);
     final newData = ref.watch(getDataProvider);
+    final streamdata = ref.watch(streamProvider);
 
     return Scaffold(
       backgroundColor: Colors.blueGrey.shade300,
@@ -26,53 +27,66 @@ class Homescreen extends ConsumerWidget {
         backgroundColor: Colors.red.shade200,
       ),
       
-      body: Column(
-        
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 20,),
-          Text("Simple Provider Watch Example : $text1"),
-          Text("Simple Provider Read Example : $text2"),
-          const SizedBox(height: 50,),
-          Center(child: Text(count.toString(), style: TextStyle(fontSize: 20),)),
-          //TextButton(onPressed: () => ref.read(countProvider.notifier).update((state) => value), child: Icon(Icons.add))
-          Row( 
-          mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(onPressed: () => updatevalue(ref, (int.tryParse(count.toString()) ?? 0) +1), child: Icon(Icons.add), ),
-              TextButton(onPressed: () => updatevalue(ref, (int.tryParse(count.toString()) ?? 0) -1), child: Icon(Icons.remove), ),
-            ],
-          ),
-
-          const SizedBox(height: 20,),
-
-          Center(
-            child: result.when(data: (joke) => Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Column(
+          
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20,),
+            Text("Simple Provider Watch Example : $text1"),
+            Text("Simple Provider Read Example : $text2"),
+            const SizedBox(height: 50,),
+            Center(child: Text(count.toString(), style: TextStyle(fontSize: 20),)),
+            //TextButton(onPressed: () => ref.read(countProvider.notifier).update((state) => value), child: Icon(Icons.add))
+            Row( 
+            mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Joke type: ${joke.type}"),
-                const SizedBox(height: 10,),
-                Text("Joke PunchLine : ${joke.punchline}"),
-                const SizedBox(height: 10,),
-                Text("Joke ${joke.setup}"),
+                TextButton(onPressed: () => updatevalue(ref, (int.tryParse(count.toString()) ?? 0) +1), child: Icon(Icons.add), ),
+                TextButton(onPressed: () => updatevalue(ref, (int.tryParse(count.toString()) ?? 0) -1), child: Icon(Icons.remove), ),
               ],
-            ), error: (error, stackTrace) => Text("Error: $error"), loading: () => const CircularProgressIndicator()),
-          ),
-
-          const SizedBox(height: 25,),
-          Center(
-              child: newData.when(data: (dog) => Column(
+            ),
+        
+            const SizedBox(height: 20,),
+        
+            Center(
+              child: result.when(data: (joke) => Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.network(dog.message),
+                  Text("Joke type: ${joke.type}"),
                   const SizedBox(height: 10,),
-                  Text(dog.status)
+                  Text("Joke PunchLine : ${joke.punchline}"),
+                  const SizedBox(height: 10,),
+                  Text("Joke ${joke.setup}"),
                 ],
-              ), error: (error, stackTrace) => Text("Error : $error"), loading: () => const CircularProgressIndicator()),
-          )
-        ],
+              ), error: (error, stackTrace) => Text("Error: $error"), loading: () => const CircularProgressIndicator()),
+            ),
+        
+            const SizedBox(height: 25,),
+            Center(
+                child: newData.when(data: (dog) => Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.network(dog.message),
+                    const SizedBox(height: 10,),
+                    Text(dog.status)
+                  ],
+                ), error: (error, stackTrace) => Text("Error : $error"), loading: () => const CircularProgressIndicator()),
+            ),
+            const SizedBox(height: 25,),
+            Center(
+              child: streamdata.when(data: (data) {
+                return Text(data.toString());
+              }, error: (error, stackTrace) {
+                return Text(error.toString());
+              }, loading: () { 
+               return Center(child: CircularProgressIndicator(),);
+              } ),
+            )
+            
+          ],
+        ),
       ),
       
     );
