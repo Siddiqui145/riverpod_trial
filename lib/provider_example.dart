687c97b077d9d62aea5672e90cc7b3f1ccc08e86
Siddiqui Taha <tahasiddiqui541@gmail.com>
2024-12-17 15:53:12 +0530

    StateProvider Implemented

--- lib/provider_example.dart
+++ lib/provider_example.dart
@@ -1,3 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final readprovider = Provider<String>((ref) =>  "Hello, Riverpod");
+
+final countProvider = StateProvider<int?>((ref) => null );
